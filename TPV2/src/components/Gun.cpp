0142a80dc143e26deadc5114922c2be02dd385b9
ClaudiaZarzuela <100291375+ClaudiaZarzuela@users.noreply.github.com>
2023-02-26 21:02:55 +0100

    pinga

--- TPV2/src/components/Gun.cpp
+++ TPV2/src/components/Gun.cpp
@@ -1,1 +1,32 @@
 #include "Gun.h"
+#include "../ecs/Entity.h"
+#include "../ecs/Manager.h"
+#include "Transform.h"
+#include "Image.h"
+
+Gun::~Gun(){
+
+}
+
+void Gun::instanciateBullet() {
+	Entity* bullet = mngr_->addEntity(ecs::_grp_BULLETS);
+	Vector2D bPos = ent_->getComponent<Transform>()->getPos()
+		+ Vector2D(ent_->getComponent<Transform>()->getW() / 2.0f, ent_->getComponent<Transform>()->getH() / 2.0f)
+		- Vector2D(0.0f, ent_->getComponent<Transform>()->getH() / 2.0f + 5.0f + 12.0f).rotate(ent_->getComponent<Transform>()->getRot())
+		- Vector2D(2.0f, 10.0f);
+	Vector2D bVel = Vector2D(0.0f, -1.0f).rotate(ent_->getComponent<Transform>()->getRot()) * (ent_->getComponent<Transform>()->getVel().magnitude() + 5.0f);
+	bullet->addComponent<Transform>(bPos, bVel, 5, 20, ent_->getComponent<Transform>()->getRot()); 
+	bullet->addComponent<Image>(game->getTexture(NAVE));
+
+}
+void Gun::update() {
+	if (shoot && input_->isKeyDown(SDLK_s)) {
+		instanciateBullet();
+		shoot = false;
+		elapsedTime = sdlutils().currRealTime();
+	}
+
+	if (elapsedTime > 250) {
+
+	}
+}
--- TPV2/src/components/Gun.h
+++ TPV2/src/components/Gun.h
@@ -1,8 +1,23 @@
 #pragma once
 #include "../ecs/Component.h"
 #include "../game/ecs_def.h"
-class Gun
+#include "../sdlutils/InputHandler.h"
+class Gun : public Component
 {
+public: 
+	/* pulsando S añade una bala al juego y reproduce el sonido fire.wav. Se 
+	puede disparar sólo una bala cada 0.25sec (usar sdlutils().currTime()
+	para consultar el tiempo actual).*/
+	constexpr static ecs::cmpId_type id = ecs::_GUN;
+	Gun() {};
+	virtual ~Gun();
+	//void initComponent() override;
+	void update() override;
+	InputHandler* input_ = InputHandler::instance();
+private:
+	bool shoot = true;
+	int elapsedTime;
+	void instanciateBullet();
 
 };
 
